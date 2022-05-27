747abd53f2cd046c21be0182dbbb8acac7020ebd
Mehtab Singh <singhmehtab571@gmail.com>
2022-05-27 13:53:23 -0400

    FIRST COMMIT

---
+++ src/main/java/com/asa/vaxsys/VaxsysApplication.java
@@ -0,0 +1,13 @@
+package com.asa.vaxsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaxsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asa/vaxsys/VaxsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asa.vaxsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
