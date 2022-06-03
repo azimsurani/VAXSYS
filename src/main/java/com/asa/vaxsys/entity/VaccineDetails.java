004c77fc3f98c78e60a9fe23c51b5fae2e1f1b4f
azimsurani <azimsurani786@gmail.com>
2022-06-03 02:18:03 -0400

    Adding Vaccine Details Model

---
+++ src/main/java/com/asa/vaxsys/entity/VaccineDetails.java
@@ -0,0 +1,49 @@
+package com.asa.vaxsys.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Vaccine Details Model
+ */
+@Entity
+@Table(name = "vaccine_details")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VaccineDetails {
+
+	@JsonProperty("name")
+	@Column(unique = true)
+    private String name;
+
+    @JsonProperty("eligibility_age_start")
+    private Integer eligibilityAgeStart;
+    
+    @JsonProperty("eligibility_age_end")
+    private Integer eligibilityAgeEnd;
+
+    @JsonProperty("number_of_doses")
+    private Integer numberOfDoses;
+    
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("applicable_disease")
+    private List<String> applicableDisease;
+
+    @JsonProperty("vaccine_type")
+    private String vaccine_type;
+    
+}
