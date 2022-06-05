ddb5e4f221f0b8642111346f4915f5b2046f92a9
azimsurani <azimsurani786@gmail.com>
2022-06-05 00:24:36 -0400

    Adding Vaccine Helper Class

---
+++ src/main/java/com/asa/vaxsys/helpers/VaccineHelper.java
@@ -0,0 +1,39 @@
+package com.asa.vaxsys.helpers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.asa.vaxsys.dto.VaccineCreationRequestDto;
+import com.asa.vaxsys.entity.VaccineDetails;
+import com.asa.vaxsys.service.VaccineService;
+
+@Service
+public class VaccineHelper {
+	
+	private VaccineService vaccineService;
+
+
+    /**
+     * Instantiates a new Vaccine helper.
+     *
+     * @param vaccineService the vaccine service
+     */
+    @Autowired
+    public VaccineHelper(VaccineService vaccineService){
+        this.vaccineService = vaccineService;
+    }
+
+    
+    public VaccineDetails createVaccine(VaccineCreationRequestDto vaccineCreationRequestDto){
+    	
+        return vaccineService.createVaccine(vaccineCreationRequestDto.getName(), 
+        		vaccineCreationRequestDto.getEligibilityAgeStart(), 
+        		vaccineCreationRequestDto.getEligibilityAgeEnd(),
+        		vaccineCreationRequestDto.getNumberOfDoses(),
+        		vaccineCreationRequestDto.getDescription(),
+        		vaccineCreationRequestDto.getApplicableDisease(),
+        		vaccineCreationRequestDto.getVaccine_type());
+        
+    }
+
+}
