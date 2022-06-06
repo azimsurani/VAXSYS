d7158a5e558a8c523300cb86e13c01bc09dfe93c
sdesk260 <sdesk260@EV-SD-PT.ENCS.concordia.ca>
2022-06-06 15:28:43 -0400

    Adding Vaccine Center Service

---
+++ src/main/java/com/asa/vaxsys/service/VaccineCenterService.java
@@ -0,0 +1,52 @@
+package com.asa.vaxsys.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.asa.vaxsys.entity.VaccineCenter;
+import com.asa.vaxsys.entity.VaccineDetails;
+import com.asa.vaxsys.repository.VaccineCenterRepository;
+import com.asa.vaxsys.repository.VaccineRepository;;
+
+@Service
+public class VaccineCenterService {
+
+	private VaccineCenterRepository vaccineCenterRepository;
+
+	public VaccineCenterService(VaccineCenterRepository vaccineCenterRepository){
+		this.vaccineCenterRepository = vaccineCenterRepository;
+	}
+
+
+	
+	public VaccineCenter createVaccine(String name,String contactNumber,String address,String workingHours,String workingDays){
+
+		VaccineCenter vaccineCenter = VaccineCenter.builder()
+				.name(name)
+				.contactNumber(contactNumber)
+				.address(address)
+				.workingHours(workingHours)
+				.workingDays(workingDays)
+				.build();
+
+		vaccineCenterRepository.save(vaccineCenter);
+
+		return vaccineCenter;
+	}
+
+
+	public List<VaccineCenter> getAllVaccineCenters() {
+		// TODO Auto-generated method stub
+
+		List<VaccineCenter> allVaccineCenters = new ArrayList<>();
+
+		for(VaccineCenter center : vaccineCenterRepository.findAll()) {
+			allVaccineCenters.add(center);
+		}
+
+		return allVaccineCenters;
+	}
+
+}
