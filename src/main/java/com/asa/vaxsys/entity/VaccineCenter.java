675a9268c0d43e83f8ecaa0bb3141ac748e8175c
sdesk260 <sdesk260@EV-SD-PT.ENCS.concordia.ca>
2022-06-06 15:12:22 -0400

    Adding vaccine center entity model

---
+++ src/main/java/com/asa/vaxsys/entity/VaccineCenter.java
@@ -0,0 +1,42 @@
+package com.asa.vaxsys.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Vaccine Details Model
+ */
+@Entity
+@Table(name = "vaccine_center")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VaccineCenter extends BaseModel{
+	
+	@JsonProperty("name")
+	@Column(unique = true)
+    private String name;
+
+    @JsonProperty("contact_number")
+    private Integer contactNumber;
+     
+    @JsonProperty("description")
+    @Column(columnDefinition="text")
+    private String address;
+
+    @JsonProperty("working_hours")
+    private String workingHours;
+    
+    @JsonProperty("working_days")
+    private String workingDays;
+    
+}
