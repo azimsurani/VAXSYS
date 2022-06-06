bfa75655164fc08a9d8f183d0e3fe10bbae6c7be
Manjit110 <49787726+Manjit110@users.noreply.github.com>
2022-06-06 15:08:20 -0400

    Add files via upload

---
+++ src/main/java/com/asa/vaxsys/constants/UriConstants.java
@@ -0,0 +1,19 @@
+package com.asa.vaxsys.constants;
+
+/**
+ * The type Uri constants.
+ */
+public class UriConstants {
+
+    /**
+     * The constant registerUser.
+     */
+    public static final String registerUser = "/register_user";
+    
+    public static final String CREATE = "/create";
+    
+    public static final String GET_ALL_DATA = "/get_all_data";
+
+    public static final String registerInventory = "/register_inventory";
+
+}
